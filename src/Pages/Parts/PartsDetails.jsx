11d4761b5dcf78b9c2428dfd6fc12257dc38b962
sadik <po480373@gmai.com>
2022-05-28 15:11:09 +0600

    quantity functionality added

--- src/Pages/Parts/PartsDetails.jsx
+++ src/Pages/Parts/PartsDetails.jsx
@@ -2,13 +2,18 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Spinner from "../Shared/Spinner/Spinner";
 import "./PartsDetails.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { toast } from "react-toastify";
+
 const InventoryDetails = () => {
   const { itemId } = useParams();
+  const [user, error] = useAuthState(auth);
   const [detail, setDetail] = useState({});
+  const [d, setD] = useState(0);
   const [loading, setLoading] = useState(false);
   const [loading1, setLoading1] = useState(false);
-  const { name, img, description, price, available, supplier } = detail;
-  console.log(itemId);
+  const { name, img, description, price, available, minQty } = detail;
   const url = `http://localhost:5001/parts/${itemId}`;
 
   useEffect(() => {
@@ -25,61 +30,61 @@ const InventoryDetails = () => {
   }
 
   //* =========== deliver funcion =====================
-  const handleDeliver = () => {
-    const newQuantity = Number(available) - 1;
-    console.log(newQuantity);
-    const data = { available: newQuantity };
-    console.log(data);
+  const handleDeliver = (e) => {
+    e.preventDefault();
+    const number = Number(e.target.number.value);
+    console.log(number);
     //* send data to the server
-    fetch(url, {
-      method: "PUT",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("success", data);
-        //* load updated data
-        setLoading(true);
-        fetch(url)
-          .then((res) => res.json())
-          .then((data) => {
-            setDetail(data);
-            setLoading(false);
-          });
-      });
+    // fetch(url, {
+    //   method: "PUT",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log("success", data);
+    //     //* load updated data
+    //     setLoading(true);
+    //     fetch(url)
+    //       .then((res) => res.json())
+    //       .then((data) => {
+    //         setDetail(data);
+    //         setLoading(false);
+    //       });
+    //   });
   };
 
   //* =============== restock function ================
   const handleAdd = (e) => {
     e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
     const number = e.target.number.value;
-    const newQuantity = Number(quantity) + Number(number);
-    const data = { available: newQuantity };
-    console.log(data);
+
+    console.log(name, email, number);
     //* send data to the server
-    fetch(url, {
-      method: "PUT",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("success", data);
-        e.target.reset();
-        //* load updated data
-        setLoading(true);
-        fetch(url)
-          .then((res) => res.json())
-          .then((data) => {
-            setDetail(data);
-            setLoading(false);
-          });
-      });
+    // fetch(url, {
+    //   method: "PUT",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log("success", data);
+    //     e.target.reset();
+    //     //* load updated data
+    //     setLoading(true);
+    //     fetch(url)
+    //       .then((res) => res.json())
+    //       .then((data) => {
+    //         setDetail(data);
+    //         setLoading(false);
+    //       });
+    //   });
   };
   return (
     <div>
@@ -91,40 +96,54 @@ const InventoryDetails = () => {
           <h2>{name}</h2>
           <p className="detail-text">item id: {itemId}</p>
           <p className="detail-text">price: ${price}</p>
+          <p className="detail-text">minimum quantity to order: {minQty}</p>
           <p className="detail-text">
             available : {loading ? "updating..." : available}
           </p>
-          <p className="detail-text">
+          {/* <p className="detail-text">
             description : <small>{description}</small>
-          </p>
+          </p> */}
         </div>
       </div>
       {/* 
       //* ============= deliver service =======================
       */}
-      <button
-        style={{
-          display: "block",
-          margin: "0 auto",
-          background: "#000",
-          color: "white",
-          padding: "0 2rem",
-        }}
-        disabled={available < 1}
-        onClick={handleDeliver}
-      >
-        <p> Deliver</p>
-      </button>
+      {/* <form onSubmit={handleDeliver}>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <input type="number" name="number" pattern="^[0-9]" min="1" />
+          <input
+            type="submit"
+            value="order"
+            style={{
+              display: "block",
+              //   background: "#000",
+              //   color: "white",
+              padding: " 1rem",
+            }}
+          ></input>
+        </div>
+      </form> */}
       {/* 
       //* ============ restock form ==================
       */}
       <div className="register-form">
         <form onSubmit={handleAdd}>
-          <input type="number" name="number" pattern="^[0-9]" min="1" />
+          <input type="text" name="name" value={user?.displayName} disabled />
+
+          <input type="email" name="email" value={user?.email} disabled />
+          <input type="text" name="address" placeholder="your address" />
+          <input
+            type="number"
+            name="number"
+            pattern="^[0-9]"
+            max={available}
+            min={minQty}
+          />
           <input
             style={{ background: "#000", color: "white" }}
             type="submit"
-            value="restock"
+            value="Order"
+            placeholder="quantity"
           />
         </form>
       </div>
