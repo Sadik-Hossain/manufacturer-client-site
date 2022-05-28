d5e75c5e6aebabe4394e7b784e98af1e1686a1ad
sadik <po480373@gmai.com>
2022-05-28 10:30:04 +0600

    spinner added along with require auth

---
+++ src/Pages/Purchase/Purchase.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Purchase = () => {
+  return (
+    <div>
+      <h1>purchase page</h1>
+    </div>
+  );
+};
+
+export default Purchase;
---
+++ src/Pages/Shared/Spinner/Spinner.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Spinner.css";
+const Spinner = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Spinner;
